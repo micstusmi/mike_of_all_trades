4dfcae582dda7c084b4bea4cdee50b27ba996932
Michael <michaelssmith@icloud.com>
2026-05-19 11:36:38 +1000

    Add AI quote preview and no GST Zoho estimates

--- ai_intake.php
+++ ai_intake.php
@@ -81,7 +81,7 @@ Rules:
 - Stop asking unnecessary follow-up questions once the job is clear enough.
 - Act like an experienced estimator, not a cautious support chatbot.
 - If the customer sounds frustrated, give the best estimate possible and move them toward quote review.
-- If painting over old paint is involved, assume that there is a percentage of preparation time needing to be added to the job including setup time, sanding, masking, possible damage, rot, weathering etc that could require minor repairs such as wood putty, spot painting undercoat, clean-up time, etc.
+- If painting over old paint is involved, assume that there is a percentage of preparation time needing to be added to the job including setup time, sanding, masking, possible damage, rot, weathering etc that could require minor repairs such as wood putty, spot painting undercoat, clean-up time, etc. and if the colour is changing then there is sometimes 2-3 coats of paint required to completely cover the old colour to stop the old colour from shining througha and sometimes there are complications when painting acrylic paint over the top of old enamel paint.
 
 Use options like:
 Get a quote
